494bfddf1ee17f4ca955c6ee67ef5425e091c9d5
Mateusz <dewolaj881@gmail.com>
2020-03-07 16:12:40 +0100

    feat(navbar.tsx, tailwind.config.js, signupbutton.tsx, signinbutton.tsx): rebuilded navbar
    rebuilded navbar, providing new login and sign up buttons, replacing logo and navigation type

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from "react"
 import { css } from "@emotion/core"
 import tw from "tailwind.macro"
-import { ReactComponent as ArrowRight } from "../images/icon-arrow-right.svg"
 import { ReactComponent as Logo } from "../images/starwatch.svg"
 import Hamburger from "../components/hamburger"
+import { ReactComponent as HamburgerIcon } from "../images/icon-menu.svg"
+import SignInButton from "./signInButton"
+import SignUpButton from "./signUpButton"
 
 type NavbarProps = {
   children: ReactNode
@@ -18,70 +20,47 @@ const Navbar: React.FC<NavbarProps> = () => {
     >
       <div
         css={css`
-          ${tw`items-center justify-between hidden px-6 text-white bg-black lg:flex md:flex xl:flex`}
+          ${tw`items-center justify-between hidden px-6 text-black bg-white border-b border-black lg:flex md:flex xl:flex`}
         `}
       >
-        <div>
-          <Logo
-            css={css`
-              ${tw`w-12 h-12 ml-8`}
-            `}
-          />
-        </div>
-
         <div
           css={css`
-            ${tw`h-16`}
+            ${tw`flex items-center justify-between`}
           `}
         >
-          <ul
+          <div
             css={css`
-              ${tw`flex justify-between h-16 m-0`}
+              ${tw`w-8 h-8 fill-current`}
             `}
           >
-            <li
-              css={css`
-                ${tw`flex items-center h-16 px-10 text-lg tracking-wide`}
-              `}
-            >
-              About
-            </li>
-            <li
-              css={css`
-                ${tw`flex items-center h-16 px-10 text-lg tracking-wide`}
-              `}
-            >
-              Projects
-            </li>
-            <li
-              css={css`
-                ${tw`flex items-center h-16 px-10 text-lg tracking-wide`}
-              `}
-            >
-              Life
-            </li>
-            <li
-              css={css`
-                ${tw`flex items-center h-16 px-10 text-lg tracking-wide`}
-              `}
-            >
-              Contact
-            </li>
-            <li
-              css={css`
-                ${tw`flex items-center h-16 px-10 text-lg tracking-wide`}
-              `}
-            >
-              Jobs
-            </li>
-          </ul>
-        </div>
-        <div>
-          <ArrowRight
+            <HamburgerIcon></HamburgerIcon>
+          </div>
+          <div
             css={css`
-              ${tw`fill-current`}
+              ${tw`ml-4 text-2xl`}
             `}
-          />
+          >
+            StarWatch
+          </div>
+        </div>
+        <div
+          css={css`
+            ${tw`flex items-center h-16 pl-24`}
+          `}
+        >
+          <Logo
+            css={css`
+              ${tw`w-12 h-12`}
+            `}
+          ></Logo>
+        </div>
+        <div
+          css={css`
+            ${tw`flex items-center justify-between pr-10`}
+          `}
+        >
+          <SignInButton></SignInButton>
+          <SignUpButton></SignUpButton>
         </div>
       </div>
       <div
